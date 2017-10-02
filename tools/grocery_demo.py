fc4f11e5d87c49981b6347edeef9161f2e3fe7aa
vidit09 <vidit7@outlook.com>
2017-10-02 15:39:42 +0200

    minor

--- tools/grocery_demo.py
+++ tools/grocery_demo.py
@@ -94,7 +94,7 @@ def demo(sess, net, image_name):
     print('Detection took {:.3f}s for {:d} object proposals'.format(timer.total_time, boxes.shape[0]))
 
     # Visualize detections for each class
-    CONF_THRESH = 0.8
+    CONF_THRESH = 0.5
     NMS_THRESH = 0.3
     for cls_ind, cls in enumerate(CLASSES[1:]):
         cls_ind += 1 # because we skipped background
