59cfa2a535bcf867248eeab0d4a4c12f38c4eb9f
vidit09 <vidit7@outlook.com>
2018-02-09 16:10:23 +0100

    minor

--- lib/nets/network.py
+++ lib/nets/network.py
@@ -249,7 +249,7 @@ class Network(object):
       if cfg.POOLING_MODE == 'crop':
         pool5 = self._crop_pool_layer(net_conv, rois, "pool5")
 
-        # self.ten = tf.get_default_graph().get_tensor_by_name('resnet_v1_101/block2/unit_4/bottleneck_v1/conv3/ReLu:0')
+        self.ten = tf.get_default_graph().get_tensor_by_name('resnet_v1_101_2/block2/unit_4/bottleneck_v1/ReLu:0')
         self.smcrop = self._crop_pool_layer(net_conv,self._gt_smboxes,"smcrop")
         self._box_diversity_fn(rois,self.smcrop,"box_diversity_fn")
       else:
@@ -446,9 +446,9 @@ class Network(object):
 
     layers_to_output = {'rois': rois}
 
-    for op in tf.get_default_graph().get_operations():
-      if 'block2' in op.name:
-        print(op.name)
+    # for op in tf.get_default_graph().get_operations():
+    #   if 'block2' in op.name:
+    #     print(op.name)
 
     for var in tf.trainable_variables():
       self._train_summaries.append(var)
